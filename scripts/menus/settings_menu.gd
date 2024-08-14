ec93d8c28c99b95bafa05f22577309568b2189b3
Spirit <spirit@programmer.net>
2024-08-13 21:51:48 -0700

    add game settings tab

--- scripts/menus/settings_menu.gd
+++ scripts/menus/settings_menu.gd
@@ -1,12 +1,20 @@
 extends Node
 
 
+const game_tab = preload("res://scenes/menus/game_settings_tab.tscn")
+
+
 var game_config
 
 
 func _ready():
 	sync_settings()
 	Settings.connect("settings_updated", sync_settings)
+	
+	if game_config != null:
+		$TabContainer.add_child(game_tab.instantiate())
+		sync_game_settings()
+		game_config.connect("game_settings_updated", sync_game_settings)
 
 
 func back():
@@ -18,6 +26,10 @@ func back():
 
 func sync_settings():
 	%power_saving.button_pressed = Settings.power_saving
+
+
+func sync_game_settings():
+	pass
 
 
 func toggle_power_saving(is_toggled_on: bool):
